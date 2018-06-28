d68f315123bc78ea32980d450d1e94b7e9eefa5e
Hopingocean <you@example.com>
2018-06-28 15:40:12 +0800

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+	title: 'VuePress',
+	description: 'A VuePress Demo',
+	themeConfig: {
+		nav: [
+			{ text: 'guide', link: '/guide/' },
+			{ text: 'foo', link: '/foo/' },
+			{ text: 'bar', link: '/bar/'},
+			{
+				text: 'Languages',
+				items: [
+				  { text: 'Chinese', link: '/language/chinese' },
+				  { text: 'Japanese', link: '/language/japanese' }
+				]
+			}
+		],
+		sidebar: {
+      '/foo/': [
+        '',     /* /foo/ */
+        'one',  /* /foo/one.html */
+        'two'   /* /foo/two.html */
+      ],
+
+      '/bar/': [
+        '',      /* /bar/ */
+        'three', /* /bar/three.html */
+        'four'   /* /bar/four.html */
+			],
+			
+      '/guide/': [
+				{
+					title: 'Group 1',
+					collapsable: false,
+					children: [
+						'',        /* / */
+						'contact',
+						'about'
+					]
+				},
+        {
+					title: 'Group 2',
+					children: []
+				},
+			],
+		},
+		// displayAllHeaders: true,
+	}
+}
---
+++ bar/README.md
@@ -0,0 +1,2 @@
+# bar
+a bar page
